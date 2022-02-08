d9f03cb8a50780e37af7cc68ef2ceee7195e8ad8
Krzysztof <kkarpiski@yahoo.com>
2022-02-08 23:43:23 +0100

    Mod getGiosData and add method for read data from gios

--- src/main/java/pl/kkarpinski/association_rules/service/GiosService.java
+++ src/main/java/pl/kkarpinski/association_rules/service/GiosService.java
@@ -6,10 +6,12 @@ import org.springframework.boot.configurationprocessor.json.JSONArray;
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.stereotype.Service;
-import pl.kkarpinski.association_rules.model.GiosDto;
 import pl.kkarpinski.association_rules.webClient.gios.GiosClient;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+
 
 /**
  * Created by kkarp.
@@ -23,55 +25,52 @@ public class GiosService {
 
     public String getGiosData() throws JSONException {
 
-        JSONArray jsonArray = new JSONArray(giosClient.getAllStations("id"));
-        JSONObject jsonObject = new JSONObject();
-        log.info(String.valueOf(jsonArray));
-        for (int i = 0; i < jsonArray.length(); i++) {
-            Object value = jsonArray.get(i);
-            System.out.println(value);
+        JSONArray allStationsJsonArray = new JSONArray(giosClient.getAllStations("id"));
+        log.info(String.valueOf(allStationsJsonArray));
+
+        List stationsIdList = new ArrayList();
+        List sensorsIdList = new ArrayList();
+
+        for (int i = 0; i < allStationsJsonArray.length(); i++) {
+            Object allStationValue = allStationsJsonArray.get(i);
+            JSONObject item = allStationsJsonArray.getJSONObject(i);
+            String id = item.getString("id");
+            stationsIdList.add(id);
+            
+
+            JSONObject stationIndexObj = new JSONObject(giosClient.getStationIndex(id));
+            log.info(String.valueOf(stationIndexObj));
+            Iterator stationKeys = stationIndexObj.keys();
+            while (stationKeys.hasNext()) {
+                String stationkey = (String) stationKeys.next();
+                Object stationValue = stationIndexObj.get(stationkey);
+                System.out.println(stationkey + " : " + stationValue);
 
         }
 
-        JSONArray sensorArray = new JSONArray(giosClient.findSensors("296"));
-        log.info(String.valueOf(sensorArray));
-        for (int i = 0; i < sensorArray.length(); i++) {
-            Object value = sensorArray.get(i);
-            System.out.println(value);
         }
+            for (int b = 0; b < stationsIdList.size(); b++) {
+                log.info(String.valueOf(sensorsIdList.get(b)));
+            JSONObject sensorDataObj = new JSONObject(giosClient.getSensorData((String) sensorsIdList.get(b)));
+            log.info(String.valueOf(sensorDataObj));
+            Iterator keys = sensorDataObj.keys();
+            while (keys.hasNext()) {
+                String key = (String) keys.next();
+                Object sensorValue = sensorDataObj.get(key);
+                System.out.println(key + " : " + sensorValue);
 
-        JSONObject sensorDataObj = new JSONObject(giosClient.getSensorData("2088"));
-        log.info(String.valueOf(sensorDataObj));
-        Iterator keys = sensorDataObj.keys();
-        while (keys.hasNext()) {
-            String key = (String) keys.next();
-            Object value = sensorDataObj.get(key);
-            System.out.println(key + " : " + value);
+                JSONArray sensorArray = new JSONArray(giosClient.findSensors((String) stationsIdList.get(b)));
+                log.info(String.valueOf(sensorArray));
+                for (int o = 0; o < sensorArray.length(); o++) {
+                    Object findSensorValue = sensorArray.get(o);
+                    JSONObject obj = sensorArray.getJSONObject(o);
+                    String id = obj.getString("id");
+                    sensorsIdList.add(id);
+                }
+            }
+
+            }
+            return null;
         }
-
-        JSONObject stationIndexObj = new JSONObject(giosClient.getStationIndex("296"));
-        log.info(String.valueOf(stationIndexObj));
-        Iterator stationKeys = stationIndexObj.keys();
-        while (keys.hasNext()) {
-            String stationkey = (String) stationKeys.next();
-            Object value = stationIndexObj.get(stationkey);
-            System.out.println(stationkey + " : " + value);
-        }
-
-
-        return null;
-    }
-    public GiosDto getDataFromAllStatons(){
-        return giosClient.getDataFromAllStatons();
-    }
-
-    String[] extractNames(JSONObject input) throws JSONException {
-        JSONArray objects = input.getJSONArray("id");
-        int namesCount = objects.length();
-        String[] names = new String[namesCount];
-        for (int i = 0; i < namesCount; i++) {
-            names[i] = objects.getJSONObject(i).getString("id");
-        }
-        return names;
-    }
 
 }
